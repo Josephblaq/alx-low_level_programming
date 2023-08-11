486c2603cf07f16b7afdec3292626a1da5b0aaaa
Josephblaq <josephforgod11@gmail.com>
2023-08-11 15:33:19 -0700

    4-puts.c C program file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main  C program that prints with a new line using the puts function
+ * return (0)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
