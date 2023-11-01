721c5a948db28772250b46fc031552b4412381e4
josephblaq <josephforgod11@gmail.com>
2023-11-01 15:07:38 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _putchar - prints character c to the standard output (stdout)
+ * @c: The character to be printed
+ *
+ * Return: Always return 0(success)
+ */
+int _putchar(char c)
+{
+return (0);
+}
+
